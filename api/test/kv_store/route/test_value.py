d25531d5b61f1ea43e218914cd1dfcf968f61831
Peter G Lane <muffinspawn@gmail.com>
2020-01-04 17:15:23 -0600

    Add update and get unit tests.

--- api/test/kv_store/route/test_value.py
+++ api/test/kv_store/route/test_value.py
@@ -18,3 +18,29 @@ def test_response_contains_correct_value_after_set(client):
 	assert response.json['key'] == value['key']
 	assert response.json['value'] == value['value']
 
+
+def test_response_contains_correct_value_after_update(client):
+	value = dict(key='foo', value='bar')
+	print(f"Value: {value}")
+	client.post('/api/value', json=value)
+
+	value['value'] = 'fighters'
+	key = value.pop('key')
+	response = client.put('/api/value/foo', json=value)
+
+	assert response.status_code == 200
+	assert response.json['key'] == key
+	assert response.json['value'] == value['value']
+
+
+def test_response_contains_correct_value_after_get(client):
+	value = dict(key='foo', value='bar')
+	print(f"Value: {value}")
+	client.post('/api/value', json=value)
+
+	response = client.get('/api/value/foo')
+
+	assert response.status_code == 200
+	assert response.json['key'] == value['key']
+	assert response.json['value'] == value['value']
+
